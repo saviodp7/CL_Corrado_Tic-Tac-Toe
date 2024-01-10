a3fca2c63cd7b6586c1787964068ec5e33d9162f
saviodp7 <saviodp7@gmail.com>
2024-01-10 15:59:35 +0100

    tris main node

---
+++ tris_main/script/corrado_main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import rospy
+
+from corrado_camera.msg import BestMove
+from std_msgs.msg import Int8
+
+mossa = None
+
+def callback(best_move_msg):
+    global mossa
+    mossa = best_move_msg.mossa
+    rospy.loginfo(f"\nMossa migliore: {best_move_msg.mossa}, score: {best_move_msg.score}")
+
+def main():
+    rospy.init_node("game_node", anonymous=True)
+    rate = rospy.Rate(0.5) #Hz
+
+    mossa_corrado_pub = rospy.Publisher('mossa', Int8, queue_size=1)
+    rospy.Subscriber('best_move', BestMove, callback)
+
+    while not rospy.is_shutdown(): 
+
+        input("Premi invio per eseguire la mossa visualizzata...\n")
+        if mossa != None:
+            mossa_corrado_pub.publish(mossa)
+            
+        else:
+            rospy.logerr("NESSUNA MOSSA VALIDA TROVATA!!!")
+    
+    rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
+
+
+
+
